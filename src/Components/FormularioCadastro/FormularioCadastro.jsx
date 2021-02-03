c31ec27d0d0b1a73e7f8c0d60f890faf7aabb58f
joaovictordjardim <joaovictordjardim@gmail.com>
2021-02-03 15:19:18 -0300

    Melhorando estrutura do formulario de cadastro

--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,29 +2,21 @@ import React, { useState } from 'react';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
-import {Typography} from '@material-ui/core';
 
 function FormularioCadastro({aoEnviar, validarCpf}) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf}/>,
+    <DadosEntrega aoEnviar={aoEnviar}/>
+  ]
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  function formularioAtual (etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf}/>;
-      case 2: 
-       return <DadosEntrega aoEnviar={aoEnviar}/>;
-      default: 
-        return <Typography>Error</Typography>;
-    }
-  }
-
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formularios[etapaAtual]}</>;
 }
 
 export default FormularioCadastro; 
